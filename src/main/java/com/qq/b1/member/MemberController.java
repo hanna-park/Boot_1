5f0a9dbb7a42119377242d40a1ba43921c1bf4b9
hanna-park <hannaddy@naver.com>
2020-01-02 11:46:33 +0900

    file

--- src/main/java/com/qq/b1/member/MemberController.java
+++ src/main/java/com/qq/b1/member/MemberController.java
@@ -3,7 +3,10 @@ package com.qq.b1.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/member/**")
@@ -15,6 +18,25 @@ public class MemberController {
 	@GetMapping("memberJoin")
 	public String memberJoin() throws Exception{
 		return "member/memberJoin";
+	}
+	
+	@PostMapping("memberJoin")
+	public ModelAndView memberJoin(MemberVO memberVO,MultipartFile files) throws Exception{
+		ModelAndView mv = new ModelAndView();
+		int result = memberservice.memberJoin(memberVO, files);
+		
+		String msg = "Join Fail";
+		String path = "../";
+		
+		if(result>0) {
+			msg= "Join Success";
+		}
+		
+		mv.setViewName("common/result");
+		mv.addObject("msg",msg);
+		mv.addObject("path",path);
+		
+		return mv;
 	}
 	
 }
--- src/main/java/com/qq/b1/member/MemberService.java
+++ src/main/java/com/qq/b1/member/MemberService.java
@@ -1,16 +1,34 @@
 package com.qq.b1.member;
 
+import java.io.File;
+
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.qq.b1.util.FilePathGenerator;
+import com.qq.b1.util.FileSaver;
 
 @Service
 public class MemberService {
 
 	@Autowired
 	private MemberMapper memberMapper;
+	@Autowired
+	private ServletContext servletContext;
+	@Autowired
+	private FilePathGenerator filePathGenerator;
+	@Autowired
+	private FileSaver fileSaver;
 	
-	public int memberJoin(MemberVO memberVO) throws Exception{
-		return memberMapper.memberJoin(memberVO);
+	public int memberJoin(MemberVO memberVO,MultipartFile files) throws Exception{
+		File file= filePathGenerator.getUseClassPathResource("upload");
+		String fileName = fileSaver.save(file, files);
+		System.out.println(fileName);
+		return 0; 
+		//memberMapper.memberJoin(memberVO);
 	}
 	
 }
